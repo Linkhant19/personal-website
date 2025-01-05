b6d4d282c6a22c09faf049137ac07f87d32a215e
Linkhant19 <lkk19@bu.edu>
2025-01-05 18:34:20 -0500

    trying heroku deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import * as React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 import Home from './components/Home';
 import Characters from './components/Characters';
@@ -8,20 +8,12 @@ import Contact from './components/Contact';
 
 function App() {
   return (
-    // <BrowserRouter basename="/personal-website"> 
-    //   <Routes>
-    //     <Route exact path="/personal-website" element={<Home />} />
-    //     <Route path="/characters" element={<Characters />} />
-    //     <Route path="/roadmap" element={<Roadmap />} />
-    //     <Route path="/contact" element={<Contact />} />
-    //   </Routes>
-    // </BrowserRouter>
-    <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/characters" element={<Characters />} />
-        <Route path="/roadmap" element={<Roadmap />} />
-        <Route path="/contact" element={<Contact />} />
-    </Routes>
+    <div>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/characters" element={<Characters />} />
+      <Route path="/roadmap" element={<Roadmap />} />
+      <Route path="/contact" element={<Contact />} />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <Router basename={process.env.PUBLIC_URL}>
     <App />
-  </BrowserRouter>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
